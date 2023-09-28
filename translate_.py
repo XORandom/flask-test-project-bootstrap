d429f71dad0751e9aa0056f1e3b26fe6b4a3a22f
XORandom <40043238+XORandom@users.noreply.github.com>
2023-09-28 15:26:51 +0300

    Add files via upload

--- translate_.py
+++ translate_.py
@@ -10,8 +10,3 @@ def translate(txt, source_lang, dest_lang):
     tr = Translator()
     res = tr.translate(txt, src=source_lang, dest=dest_lang)
     return res.text
-
-
-if __name__ == '__main__':
-
-    pass
