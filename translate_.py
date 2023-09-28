4af05c19036fdd49be8fa5b4a3623c0b94ca886f
XORandom <40043238+XORandom@users.noreply.github.com>
2023-09-28 22:41:38 +0300

    Add files via upload

--- translate_.py
+++ translate_.py
@@ -3,10 +3,17 @@ from googletrans import Translator
 
 def get_language(txt):
     tr = Translator()
-    return tr.detect(txt).lang
+    res = tr.detect(txt)
+    return res.lang
 
 
 def translate(txt, source_lang, dest_lang):
     tr = Translator()
+    print(txt)
     res = tr.translate(txt, src=source_lang, dest=dest_lang)
     return res.text
+
+
+if __name__ == '__main__':
+    print(get_language('0xA'))
+    #print(translate('Hello', 'en', 'ru'))
