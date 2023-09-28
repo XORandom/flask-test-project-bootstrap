48b956b096c43cf4d74f8482e2dc1e67c58e4510
XORandom <40043238+XORandom@users.noreply.github.com>
2023-09-28 13:54:46 +0300

    Add files via upload

---
+++ translate_.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+
+
+def get_language(txt):
+    tr = Translator()
+    return tr.detect(txt).lang
+
+
+def translate(txt, source_lang, dest_lang):
+    tr = Translator()
+    res = tr.translate(txt, src=source_lang, dest=dest_lang)
+    return res.text
+
+
+if __name__ == '__main__':
+
+    pass
